5cfd10a5084babf24cb3d8c7457c9bbc7e9426bd
Youssef Khaky <youssefkhaky@hotmail.com>
2018-06-17 15:24:25 -0400

    fixed bash script

--- src/service_bot/batch_scripts/test_navigation.sh
+++ src/service_bot/batch_scripts/test_navigation.sh
@@ -4,6 +4,6 @@ sleep 5
 #xterm -hold -e   " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 xterm  -e  " roslaunch service_bot amcl.launch" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm  -e  " rosrun turtlebot_teleop turtlebot_teleop_key"
